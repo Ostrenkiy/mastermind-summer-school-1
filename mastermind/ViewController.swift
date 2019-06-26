a2648ed03727cd6e6b1f9e111de8ddc790007aab
Alexander Karpov <x1te.rus@gmail.com>
2019-06-27 00:58:47 +0300

    Initial Commit

---
+++ mastermind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mastermind
+//
+//  Created by Ostrenkiy on 27/06/2019.
+//  Copyright © 2019 Ostrenkiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
